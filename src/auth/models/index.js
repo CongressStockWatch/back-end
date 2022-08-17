811ed3acec8b5109633094c6fdafad17708796c6
Brady Davenport <brady.davenport@pm.me>
2022-08-17 12:39:04 -0600

    db url

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.js');
 
-const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : 'sqlite:memory';
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? 'sqlite::memory'
+  : process.env.DATABASE_URL || 'postgres://localhost:5432/congress-stock-watch';
 
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
@@ -16,6 +18,9 @@ const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
 } : {};
 
 const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+sequelize.sync()
+  .then(() => console.log(`Successful connection to db: ${DATABASE_URL}`))
+  .catch(err => console.error(err));
 
 module.exports = {
   db: sequelize,
